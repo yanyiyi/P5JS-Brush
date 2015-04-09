4dab78afa91a3ebbfb7c98b02f1fb309aef97270
yanyiyi <chunyan.liu@yannest.net>
2015-04-10 00:59:52 +0800

    Initial Project  A p5.js brush
    Initial Project  A p5.js brush

---
+++ js/sketch.js
@@ -0,0 +1,24 @@
+    var ScaleX, ScaleY;
+    
+function setup() {
+    createCanvas(1280, 1080);
+    background(20, 120, 180);
+}
+
+function draw() {
+    ScaleX = random(20, 30);
+    ScaleY = random(20, 30);
+    //background(20, 120, 180);
+    noStroke();
+if (mouseIsPressed) {
+    fill(20, 120, 180, 50);
+  } else {
+    fill(255, 50);
+  }
+    ellipse(mouseX, mouseY, ScaleX*1, ScaleY*1);
+    ellipse(mouseX, mouseY, ScaleX*1.2, ScaleY*1.2);
+    ellipse(mouseX, mouseY, ScaleX*1.4, ScaleY*1.4);
+    ellipse(mouseX, mouseY, ScaleX*1.6, ScaleY*1.6);
+    ellipse(mouseX, mouseY, ScaleX*1.8, ScaleY*1.8);
+      ellipse(mouseX, mouseY, ScaleX*2, ScaleY*2);
+}
