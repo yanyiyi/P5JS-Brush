416a99e498ce2ed6c27f6db6a066ce049f50880e
yanyiyi <chunyan.liu@yannest.net>
2015-04-10 02:38:25 +0800

    Update Color Bug & Add Download Feature
    Update Color Bug & Add Download Feature

--- js/sketch.js
+++ js/sketch.js
@@ -10,15 +10,26 @@ function draw() {
     ScaleY = random(20, 50);
     //background(20, 120, 180);
     noStroke();
-if (mouseIsPressed) {
-    fill(20, 120, 180, 50);
-  } else {
-    fill(255, 50);
-  }
-    ellipse(mouseX, mouseY, ScaleX*1, ScaleY*1);
-    ellipse(mouseX, mouseY, ScaleX*1.2, ScaleY*1.2);
-    ellipse(mouseX, mouseY, ScaleX*1.4, ScaleY*1.4);
-    ellipse(mouseX, mouseY, ScaleX*1.6, ScaleY*1.6);
-    ellipse(mouseX, mouseY, ScaleX*1.8, ScaleY*1.8);
-      ellipse(mouseX, mouseY, ScaleX*2, ScaleY*2);
+    if (mouseIsPressed) {
+        fill(20, 120, 180, 90);
+      } else {
+        fill(255, 40);
+      }
+        ellipse(mouseX, mouseY, ScaleX*1, ScaleY*1);
+        ellipse(mouseX, mouseY, ScaleX*1.2, ScaleY*1.2);
+        ellipse(mouseX, mouseY, ScaleX*1.4, ScaleY*1.4);
+        ellipse(mouseX, mouseY, ScaleX*1.6, ScaleY*1.6);
+        ellipse(mouseX, mouseY, ScaleX*1.8, ScaleY*1.8);
+          ellipse(mouseX, mouseY, ScaleX*2, ScaleY*2);
+
+        if (keyIsPressed){
+        save('myCanvas.jpg');
+        }
+    
+    
+    
+        var s = "No Click for Draw. Click for Erase. Any KeyBoard for Save";
+        fill(255);
+        text(s, 20, 20, 500, 80); // Text wraps within text box
+
 }
