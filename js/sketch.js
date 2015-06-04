0e2b4031bdcebe1c791b33fc1365d9c439894b04
yanyiyi <chunyan.liu@yannest.net>
2015-06-04 18:18:11 +0800

    For Air
    For Air

--- js/sketch.js
+++ js/sketch.js
@@ -1,17 +1,18 @@
     var ScaleX, ScaleY;
-    
+    var brushSize = 10;
+    var colorC,colorM,colorY;
 function setup() {
     createCanvas(1280, 800);
-    background(20, 120, 180);
+    background(0);
 }
 
 function draw() {
-    ScaleX = random(20, 50);
-    ScaleY = random(20, 50);
+    ScaleX = random(20, brushSize);
+    ScaleY = random(20, brushSize);
     //background(20, 120, 180);
     noStroke();
     if (mouseIsPressed) {
-        fill(20, 120, 180, 90);
+        fill(0, 90);
       } else {
         fill(255, 40);
       }
@@ -20,16 +21,23 @@ function draw() {
         ellipse(mouseX, mouseY, ScaleX*1.4, ScaleY*1.4);
         ellipse(mouseX, mouseY, ScaleX*1.6, ScaleY*1.6);
         ellipse(mouseX, mouseY, ScaleX*1.8, ScaleY*1.8);
-          ellipse(mouseX, mouseY, ScaleX*2, ScaleY*2);
+        ellipse(mouseX, mouseY, ScaleX*2, ScaleY*2);
 
         if (keyIsPressed){
-        save('myCanvas.jpg');
-        }
+             if (keyCode == RETURN) {
+                save('myCanvas.jpg');
+             }else if(keyCode == TAB) {
+                var fs = fullscreen();
+                fullscreen(!fs);
+             }else if(keyCode == UP_ARROW){
+                brushSize += 5;
+             }else if(keyCode == DOWN_ARROW){
+                brushSize -= 5;
+             } 
+        }    
     
-    
-    
-        var s = "No Click for Draw. Click for Erase. Any KeyBoard for Save";
-        fill(255);
-        text(s, 20, 20, 500, 80); // Text wraps within text box
+       // var s = "No Click for Draw. Click for Erase. Any KeyBoard for Save";
+       // fill(255);
+       // text(s, 20, 20, 500, 80); // Text wraps within text box
 
 }
